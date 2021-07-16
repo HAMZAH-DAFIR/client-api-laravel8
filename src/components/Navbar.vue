1ffe895813475f5be8f3c420ae7e70d536eaba56
DAFIR HAMZAH <dafirhamzah@gmail.com>
2021-07-16 23:32:03 +0200

    pages and component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template lang="">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <router-link class="navbar-brand" :to="{ name : 'Home' }">VueX</router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <router-link class="nav-link " :to="{ name : 'Home' }">Home
+            <span class="visually-hidden">(current)</span>
+          </router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" :to="{ name : 'Dashbord' }">dashbord</router-link>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <router-link class="nav-link" :to="{ name : 'Singin' }">Singin</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/Singin.vue
@@ -0,0 +1,42 @@
+<template lang="">
+    
+    <div class="row">
+        <div class="row col-md-6 mx-auto mt-3">
+            <h1 class="text-center">Singin</h1>
+            <form method="POST" action="" >
+                <div class="form-group has-success">
+                    <label class="form-label mt-4" for="inputValid">Email</label>
+                    <input type="email" value="" name="email" class="form-control is-valid" id="inputValid">
+                    <div class="valid-feedback">Success! You've done it.</div>
+                  </div>
+                  <div class="form-group has-success">
+                    <label class="form-label mt-4" for="inputValid">Password</label>
+                    <input type="password" name="password" value="" class="form-control is-valid" id="inputValid">
+                    <div class="valid-feedback">Success! You've done it.</div>
+                  </div>
+                  <button type="submit" class="btn btn-outline-primary d-block text-center">Sing In</button>
+            </form>
+    
+        </div>
+
+    </div>
+    
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                user: {
+                    'email': '',
+                    'password': ''
+                }
+            }
+        },
+        methods: {
+
+        },
+    }
+</script>
+<style lang="">
+
+</style>
