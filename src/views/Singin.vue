79fd5fec0bc5a64f190dfd67bdb70e7ad03b0cfe
DAFIR HAMZAH <dafirhamzah@gmail.com>
2021-07-17 00:04:37 +0200

    send to back end with axios

--- src/views/Singin.vue
+++ src/views/Singin.vue
@@ -3,15 +3,15 @@
     <div class="row">
         <div class="row col-md-6 mx-auto mt-3">
             <h1 class="text-center">Singin</h1>
-            <form method="POST" action="" >
+            <form @submit.prevent="singin()" >
                 <div class="form-group has-success">
                     <label class="form-label mt-4" for="inputValid">Email</label>
-                    <input type="email" value="" name="email" class="form-control is-valid" id="inputValid">
+                    <input type="email" v-model="user.email" class="form-control is-valid" id="inputValid">
                     <div class="valid-feedback">Success! You've done it.</div>
                   </div>
                   <div class="form-group has-success">
                     <label class="form-label mt-4" for="inputValid">Password</label>
-                    <input type="password" name="password" value="" class="form-control is-valid" id="inputValid">
+                    <input type="password" v-model="user.password"  class="form-control is-valid" id="inputValid">
                     <div class="valid-feedback">Success! You've done it.</div>
                   </div>
                   <button type="submit" class="btn btn-outline-primary d-block text-center">Sing In</button>
@@ -23,6 +23,7 @@
     
 </template>
 <script>
+    import axios from 'axios';
     export default {
         data() {
             return {
@@ -33,7 +34,10 @@
             }
         },
         methods: {
-
+            async singin() {
+                const response = await axios.post('http://127.0.0.1:8000/api/login', this.user);
+                console.log(response.data);
+            }
         },
     }
 </script>
